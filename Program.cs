26878cf2b93ca36fc2c6e1d65e7cbb87a484899e
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-01-08 10:48:53 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Data.SqlTypes;
+
+namespace ball_tournament
+{
+    internal class Program
+    {
+        static void Squad_power(int[] a)
+        {
+           
+            for (int i = 0; i < a.Length; i++) 
+            {
+
+            a[i] = rand_roll.Next(30,100);
+
+            }
+            
+            
+        }
+        static int[] Goal_morale(int[] manchester_main)
+        {
+            for (int j = 0; j < manchester_main.Length; j++)
+            {
+                if (manchester_main[j] > 0)
+                {
+                    manchester_main[j] += 5;
+                }
+            }
+            return manchester_main;
+        }
+       static Random rand_roll=new Random();
+        static void Main(string[] args)
+        {
+            bool penalty_given = false;
+            int manchester_goals = 0, arsenal_goals = 0, event_decider = 0, manchester_yellows = 0, manchester_reds = 0,penalty_decider=0;
+            int[] manchester_main = new int[11];
+            int[] manchester_spare = new int[5];
+            int[] arsenal_main = new int[11];
+            int[] arsenal_spare = new int[5];
+            
+              Squad_power(manchester_main);
+              Squad_power(manchester_spare);
+              Squad_power(arsenal_main);
+              Squad_power(arsenal_spare);
+            for (int i = 0; i < 90; i++) {
+                event_decider = rand_roll.Next(1, 7);
+                {
+                    if (event_decider == 1)
+                    {
+                        manchester_goals++;
+                        manchester_main = Goal_morale(manchester_main);
+
+                    }
+                    else if (event_decider == 2)
+                    {
+                        while (penalty_given == false)
+                        {
+
+                            penalty_decider = rand_roll.Next(1, 7);
+                            manchester_yellows++;
+
+                            if (manchester_main[penalty_decider] >= 5)
+                            {
+                                manchester_main[penalty_decider] -= 5;
+                                penalty_given = true;
+                            }
+                            else if (manchester_main[penalty_decider] <= 5 && manchester_main[penalty_decider] > 0)
+                            {
+                                manchester_main[penalty_decider] = 0;
+                                penalty_given = true;
+                            }
+
+
+                        }
+                        penalty_given = false;
+
+                    }
+                    else if (event_decider == 3)
+                    {
+                        manchester_reds++;
+                        while (penalty_given == false)
+                        {
+                            penalty_decider = rand_roll.Next(1, 11);
+
+                            if (manchester_main[penalty_decider] != 0)
+                            {
+                                manchester_main[penalty_decider] = 0;
+                                penalty_given = true;
+                            }
+                        }
+
+
+
+
+                    }
+                    else if (event_decider == 4) {
+                    
+                    
+                    }
+                    
+                }
+            }
+        }
+    }
+}
